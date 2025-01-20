1ca4f40ba2497d244ccb2a8a9f931c70d64f5bb7
Michael Chrupcala <michael@artblocks.io>
2025-01-20 12:12:06 -0500

    get and post emails

--- src/app.js
+++ src/app.js
@@ -36,6 +36,39 @@ app.post("/nonprofits", (req, res) => {
   return res.status(200).json({ newNonprofit });
 });
 
+app.get("/emails", (req, res) => {
+  return res.status(200).json(allSentEmails);
+});
+
+app.post("/emails", (req, res) => {
+  const { name, address, to, from, subject, body } = req.body;
+  const receiver = allNonprofits.get(to);
+
+  if (!name || !address || !to || !from || !subject || !body) {
+    return res.status(400).json({
+      message:
+        "Missing data, please try again. Please include name, address, to, from, subject, and body fields.",
+    });
+  }
+
+  if (!receiver) {
+    return res.status(404).json({ message: "Nobody found at that email" });
+  }
+
+  const newEmail = {
+    name: name,
+    address: address,
+    to: to,
+    from: from,
+    subject: subject,
+    body: body,
+  };
+
+  allSentEmails.push(newEmail);
+
+  return res.status(200).json(newEmail);
+});
+
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
